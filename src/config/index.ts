e62d0e1e27cf2157010a5ca4b3f4ef113d0d7e58
Samoray <idan0609@gmail.com>
2021-02-11 17:33:33 +0200

    feat: add basic lib structure

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+const config = {
+    maxAxiosResponseContentLengthToShow: 100,
+    stringifyDefaultOptions: { showHidden: false, depth: null, colors: true, breakLength: 100 },
+};
+
+export default config;
---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+import { InspectOptions } from 'util';
+
+export interface IStringifyOptions extends InspectOptions {
+    name?: string;
+}

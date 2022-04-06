196b5a0a699e858676adc7edaf7f228637313d1f
Samoray.l337 <Samoray.l337@gmail.com>
2022-04-06 20:43:17 +0300

    fix: e2e tests

--- src/tests/e2e.spec.ts
+++ src/tests/e2e.spec.ts
@@ -1,18 +1,9 @@
 import { IStringifyOptions, createStringifyFunction } from '../index';
 
 const testConfig: IStringifyOptions = {
-    formatters: [
-        { matches: 'string', params: { maxLength: 5 } },
-        { matches: 'error', params: { maxMessageLength: 5 }, fieldsWhitelist: ['message'] },
-        { matches: 'date', params: { timezone: 'Australia/Perth' } },
-        {
-            matches: (obj: any) => obj === 'SAMO',
-            format: (_obj: any) => {
-                return 'BANU';
-            },
-        },
-    ],
+    formatters: [{ matches: 'string', params: { maxLength: 5 } }],
     inspectOptions: {
+        colors: false,
         depth: Infinity,
     },
 };
@@ -20,21 +11,27 @@ const testConfig: IStringifyOptions = {
 describe('Channel tests', () => {
     const stringify = createStringifyFunction(testConfig);
 
-    describe('basic test', () => {
+    describe('basic tests', () => {
         it('should stringify empty object', async () => {
             const result = stringify({});
 
             expect(result).toBe('{}');
         });
 
-        it('should stringify basic object', async () => {
+        it('should stringify object with one empty field', async () => {
             const result = stringify({ a: '' });
 
             expect(result).toBe("{ a: '' }");
         });
+
+        it('should stringify object with multiple fields', async () => {
+            const result = stringify({ a: 'hello', b: 'world' });
+
+            expect(result).toBe("{ a: 'hello', b: 'world' }");
+        });
     });
 
-    describe('another tests', () => {
+    describe('advaneced tests', () => {
         it('should stringify another basic object', async () => {
             const result = stringify({ b: '' });
 

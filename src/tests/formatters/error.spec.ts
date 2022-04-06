939047cf753c4200f17a9fc4c39cf58532df6b2d
Samoray.l337 <Samoray.l337@gmail.com>
2022-04-06 20:43:12 +0300

    add: error tests

--- src/tests/formatters/error.spec.ts
+++ src/tests/formatters/error.spec.ts
@@ -2,14 +2,21 @@ import { getErrorFormatter } from '../../formatters/errors/error';
 import { getFormattedObject } from '../../formatters/index';
 
 describe('Error formatters tests', () => {
-    const errorFormatter = getErrorFormatter({ matches: 'error' });
+    const errorFormatter = getErrorFormatter({ matches: 'error', params: { maxMessageLength: 5 } });
     const formatters = [errorFormatter];
 
-    describe('basic test', () => {
-        it('should stringify empty object', async () => {
-            const formattedObject = getFormattedObject({}, formatters);
+    describe('basic tests', () => {
+        it('should formatt simple Error', async () => {
+            const formattedObject = getFormattedObject(new Error(''), formatters);
 
-            expect(formattedObject).toEqual({});
+            expect(formattedObject).toEqual(expect.objectContaining({ message: '' }));
+            expect(formattedObject.stack).toBeDefined();
+        });
+
+        it('should formatt simple Error with long message', async () => {
+            const formattedObject = getFormattedObject(new Error('what a giant error message'), formatters);
+
+            expect(formattedObject).toEqual(expect.objectContaining({ message: 'what ...' }));
         });
     });
 });

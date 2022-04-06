e0ba43f35ad28f79c3df341955aa9f39af3a459e
Samoray.l337 <Samoray.l337@gmail.com>
2022-04-06 21:02:26 +0300

    remove: async from tests

--- src/tests/e2e.spec.ts
+++ src/tests/e2e.spec.ts
@@ -12,19 +12,19 @@ describe('Channel tests', () => {
     const stringify = createStringifyFunction(testConfig);
 
     describe('basic tests', () => {
-        it('should stringify empty object', async () => {
+        it('should stringify empty object', () => {
             const result = stringify({});
 
             expect(result).toBe('{}');
         });
 
-        it('should stringify object with one empty field', async () => {
+        it('should stringify object with one empty field', () => {
             const result = stringify({ a: '' });
 
             expect(result).toBe("{ a: '' }");
         });
 
-        it('should stringify object with multiple fields', async () => {
+        it('should stringify object with multiple fields', () => {
             const result = stringify({ a: 'hello', b: 'world' });
 
             expect(result).toBe("{ a: 'hello', b: 'world' }");
@@ -32,7 +32,7 @@ describe('Channel tests', () => {
     });
 
     describe('advaneced tests', () => {
-        it('should stringify another basic object', async () => {
+        it('should stringify another basic object', () => {
             const result = stringify({ b: '' });
 
             expect(result).toBe("{ b: '' }");

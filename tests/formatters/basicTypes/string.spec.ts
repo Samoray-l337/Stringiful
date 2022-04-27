19824a17586ad48cc381a3e42e0908291f7dcddb
Samoray.l337 <Samoray.l337@gmail.com>
2022-04-27 21:31:53 +0300

    add: more string tests

--- tests/formatters/basicTypes/string.spec.ts
+++ tests/formatters/basicTypes/string.spec.ts
@@ -2,14 +2,13 @@ import { getStringFormatter } from '../../../src/formatters/basicTypes/string';
 import { getFormattedObject } from '../../../src/formatters/index';
 
 describe('String formatter tests', () => {
-    const stringFormatter = getStringFormatter({ matches: 'string', params: { maxLength: 10 } });
-    const formatters = [stringFormatter];
-
     describe('basic tests', () => {
+        const basicStringFormatter = getStringFormatter({ matches: 'string', params: { maxLength: 10 } });
+
         it('simple & short string should remain the same', () => {
             const simpleString = 'hello';
 
-            const formattedString = getFormattedObject(simpleString, formatters);
+            const formattedString = getFormattedObject(simpleString, [basicStringFormatter]);
 
             expect(formattedString).toEqual(simpleString);
         });
@@ -17,7 +16,7 @@ describe('String formatter tests', () => {
         it('should format long string', () => {
             const longString = 'hello world what a long message';
 
-            const formattedString = getFormattedObject(longString, formatters);
+            const formattedString = getFormattedObject(longString, [basicStringFormatter]);
             expect(formattedString).toEqual('hello worl...');
         });
 
@@ -31,9 +30,30 @@ describe('String formatter tests', () => {
                 },
             };
 
-            const formattedObject = getFormattedObject(nastedObject, formatters);
+            const formattedObject = getFormattedObject(nastedObject, [basicStringFormatter]);
             expect(formattedObject?.a?.b?.myStr).toBeDefined();
             expect(formattedObject?.a?.b?.myStr).toEqual('hello worl...');
+        });
+    });
+
+    describe('configuration tests', () => {
+        const stringFormatter = getStringFormatter({ matches: 'string', params: { maxLength: 5, overflowSuffix: '<->' } });
+        const stringFormatterWithNoSuffix = getStringFormatter({ matches: 'string', params: { maxLength: 5, overflowSuffix: '' } });
+
+        it('should put empty suffix if configured so, when string length overflowed the allowed length ', () => {
+            const longString = 'hello world what a long message';
+
+            const formattedString = getFormattedObject(longString, [stringFormatterWithNoSuffix]);
+
+            expect(formattedString).toEqual('hello');
+        });
+
+        it('should put suffix as configured when string length overflowed the allowed length ', () => {
+            const longString = 'hello world what a long message';
+
+            const formattedString = getFormattedObject(longString, [stringFormatter]);
+
+            expect(formattedString).toEqual('hello<->');
         });
     });
 });
